db4cf8d821a4406a7296a978de7fd056b5e2b8fc
Ernesto Monroy <ehmizmg@gmail.com>
2021-12-27 18:59:46 +0100

    Rearranging to facilitate importing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pymeli',
-    version='0.0.2',
+    version='0.0.6',
     description='A nice SDK for Mercado Libre',
     long_description=long_description,
     long_description_content_type="text/markdown",
